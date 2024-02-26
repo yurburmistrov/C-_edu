b85488e6bb7253802ca566c521e14bef199c825e
Yuriy Burmistrov <yurburmistrov@gmail.com>
2024-02-26 23:04:53 +0300

    result Add hw_5:hw_5_1

--- hw_5/hw_5_1/Program.cs
+++ hw_5/hw_5_1/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string GetStringFromCharArray(char[,] array)
+{
+    string result = string.Empty; 
+    for (int i = 0; i < array.GetLength(0); i++) 
+    {
+      
+        for (int j = 0; j < array.GetLength(1); j++)
+ 
+    {
+        result = result + array[i,j];
+       
+    }
+    }
+    return result;
+}
+char[,] chars = {{'a','b','c'} , {'d','e','f'}};
+string res = GetStringFromCharArray(chars);
+Console.WriteLine(res);
