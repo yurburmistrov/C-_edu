d75a7bf597d778f2d2429f8c0b5d43c71fc1177e
Yuriy Burmistrov <yurburmistrov@gmail.com>
2024-02-27 15:49:39 +0300

    end_of_int_cert_2(Ackerman)

---
+++ int_cert/int_cert_2/Program.cs
@@ -0,0 +1,15 @@
+﻿int FuncAckerman(int n, int m)
+{
+    if (n == 0)
+
+        return m + 1;
+
+    else if (m == 0)
+
+        return FuncAckerman(n - 1, 1);
+
+    else
+        return FuncAckerman(n - 1, FuncAckerman(n, m - 1));
+}
+
+Console.WriteLine(FuncAckerman(3, 5));
