a50c4397c296e1c7635057de63589496551adaad
Yuriy Burmistrov <yurburmistrov@gmail.com>
2024-02-27 15:23:00 +0300

    end_of_int_cert_1

--- int_cert/int_cert_1/Program.cs
+++ int_cert/int_cert_1/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void ShowNumbers (int start, int end)
+{
+    // Базовый случай
+    if (start == end)
+    {
+        Console.Write(start);
+        return;
+    }
+    // Рекурсивный случай
+    Console.Write(start + " ");
+    ShowNumbers(start + 1, end);
+}
+Console.Write("Введите число M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+ShowNumbers(M, N); // start = 1, end = N
