e185d1ec387ae43435aef63deb3224b054582f4f
Yuriy Burmistrov <yurburmistrov@gmail.com>
2024-02-27 17:53:40 +0300

    end_of_int_cert_3. Add random input array

---
+++ int_cert/int_cert_3/Program.cs
@@ -0,0 +1,28 @@
+﻿int[] CreateArray(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(10);
+    }
+    return array;
+}
+
+void PrintReversArray(int[] array, int j)
+{
+    if (j == 0)
+    {
+        Console.Write(array[j]);
+        return;
+    }
+    Console.Write(array[j] + "; ");
+    PrintReversArray(array, j - 1);
+}
+
+Console.Write("Введите размер массива: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int[] res = CreateArray(N);
+int j = N - 1;
+Console.WriteLine($"Массив:  {string.Join("; ", res)} ");
+Console.Write("Реверс массива: ");
+PrintReversArray(res, j);
